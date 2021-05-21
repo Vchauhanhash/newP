7918c0bd32f0ce01d4382b2990c1bc8ba88de175
Vchauhanhash <vivekchauhan052@gmail.com>
2021-05-21 10:02:03 +0530

    initial commit

--- listings/views.py
+++ listings/views.py
@@ -1,18 +1,26 @@
+from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, render
 
 from .models import Listing
 
+
 def index(request):
-  listings = Listing.objects.all()
+    listings = Listing.objects.all()
 
-  context = {
-    'listings': listings
-  }
+    paginator = Paginator(listings, 3)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
 
-  return render(request, 'listings/listings.html', context)
+    context = {
+        'listings': paged_listings
+    }
+
+    return render(request, 'listings/listings.html', context)
+
 
 def listing(request):
-  return render(request, 'listings/listing.html')
+    return render(request, 'listings/listing.html')
+
 
 def search(request):
-  return render(request, 'listings/search.html')
+    return render(request, 'listings/search.html')
